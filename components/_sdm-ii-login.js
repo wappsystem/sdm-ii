366ee92201917943159c72758e17e7412b5074cd
Gunnar Unger <gunnar.unger@gmail.com>
2019-05-23 16:07:41 +1000

    update

--- components/_sdm-ii-login.js
+++ components/_sdm-ii-login.js
@@ -1,6 +1,6 @@
 (function(){
     var modules={
-        "home":                         {"url":"$A/modules/mdb/o/online-questionnaire-login.html","Table":"spardac-participant",
+        "home":                         {"url":"$A/modules/mdb/o/online-questionnaire-login.html","Table":"sdm-ii-participant",
                                                 questionnaire_panel:"online-questionnaire-panel"
                                         }   ,
         "online-questionnaire-panel":   {url:"$A/modules/mdb/o/online-questionnaire-panel.html"},
